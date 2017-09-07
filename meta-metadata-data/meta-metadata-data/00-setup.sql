c0e3d559018ff63f15216fbfeb6e5c2a68506d2e
Alberto Federico Turelli <bibe74@users.noreply.github.com>
2017-09-07 17:46:05 +0200

    Created solution meta-metadata-data Added script 00-setup.sql

---
+++ meta-metadata-data/meta-metadata-data/00-setup.sql
@@ -0,0 +1,11 @@
+/*
+ * file: 00-setup.sql
+
+ * Restores an original copy of WideWorldImporters sample database (download from https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0)
+ * Creates an empty database named WWIDB
+
+*/
+
+USE master;
+GO
+
