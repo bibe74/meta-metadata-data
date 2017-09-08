72cfd4161fd1005cbd933b6573bd298fe4ff7beb
Alberto Federico Turelli <bibe74@users.noreply.github.com>
2017-09-08 17:47:33 +0200

    Restore WideWorldImporters database

--- meta-metadata-data/meta-metadata-data/00-setup.sql
+++ meta-metadata-data/meta-metadata-data/00-setup.sql
@@ -1,11 +1,36 @@
 /*
  * file: 00-setup.sql
+*/
 
- * Restores an original copy of WideWorldImporters sample database (download from https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0)
- * Creates an empty database named WWIDB
-
+/* Download the WideWorldImporters sample database from Microsoft's GitHub page: https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+This file contains sample TSQL to restore the file WideWorldImporters-Full.bak
+Notes & Warnings:
+    This will BLOW AWAY any existing database named WideWorldImporters.
+    Adjust the location of the backup file to the location you stored it on your test instance.
+    This is suitable for dedicated test instances only.
 */
 
 USE master;
 GO
 
+/* If the database doesn't exist, create it */
+IF DB_ID('WideWorldImporters') IS NULL
+BEGIN
+    CREATE DATABASE WideWorldImporters;
+END
+GO
+
+/* If the database exists (it should!), kick everyone out */
+IF DB_ID('WideWorldImporters') IS NOT NULL
+BEGIN
+    ALTER DATABASE WideWorldImporters SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+END
+GO
+
+/* Restore - and replace if it exists */
+RESTORE DATABASE WideWorldImporters
+    --FROM DISK='S:\MSSQL\Backup\WideWorldImporters-Full.bak'
+    FROM DISK='C:\Program Files\Microsoft SQL Server\MSSQL13.SQL2016\MSSQL\Backup\WideWorldImporters-Full.bak'
+    WITH REPLACE,
+    STATS=10;
+GO
